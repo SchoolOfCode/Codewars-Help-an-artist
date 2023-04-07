0407b125deea401d747deae6b8c9641709575e71
Carrie Shen <carrieshen@Carries-MacBook-Pro.local>
2023-04-07 17:29:20 +0100

    Step 2 complete, ready for victim to play

--- main.js
+++ main.js
@@ -1,5 +1,44 @@
 /* 
 👉 Write your kata here!
+Carrie is an artist who's very spontaneous. 
+She always starts/ works on/ stops different projects from day to day.
+Or, she comes up with new ideas but doesn't actually do it.
+
+Unfortunately, you are her assistant.
+Make a function that updates her progress list as days pass by.
+Keep track of only the projects that are still in progress.
+The number represents the percentage of the project that is in progress.
 */
 
+
+// the initial progress list
+const carriesProgress = {
+    "filmShortStoryLong" : 99,
+    "filmScotland": 1,
+    "newBook": 5,
+    "newAlbum": 20
+}
+
+// example day
+const day1 = {
+    "filmShortStoryLong" : 99,
+    "newBook": 10, 
+    "newAlbum": 0, 
+    "danceClass": 0, 
+    "newApp": 0
+}
+
+//Answer
+const expectedDay1 = {
+    "filmShortStoryLong" : 99,
+    "filmScotland": 1,
+    "newBook": 10
+}
+
 //👉 Write the function your CodeWarriors will start with below here:
+
+export function updateProgress(day) {
+    // your code here
+    
+}
+
--- main.test.js
+++ main.test.js
@@ -1,1 +1,52 @@
 //👉 Write your tests below here:
+
+// import { test, expect } from "@jest/globals";
+import { updateProgress } from "./main.js";
+
+
+// the initial progress list
+const carriesProgress = {
+    "filmShortStoryLong" : 99,
+    "filmScotland": 1,
+    "newBook": 5,
+    "newAlbum": 20
+}
+
+const day1 = {
+    "filmShortStoryLong" : 99,
+    "newBook": 10, 
+    "newAlbum": 0, 
+    "danceClass": 0, 
+    "newApp": 0
+}
+
+const expectedDay1 = {
+    "filmShortStoryLong" : 99,
+    "filmScotland": 1,
+    "newBook": 10
+}
+
+const day2 = {
+    "filmShortStoryLong" : 0,
+    "newBook": 35, 
+    "newAlbum": 25, 
+    "danceClass": 5, 
+    "newApp": 30,
+}
+
+const expectedDay2 = {
+    "filmScotland": 1,
+    "newBook": 35,
+    "newAlbum": 25,
+    "danceClass": 5,
+    "newApp": 30
+}
+
+
+test("When given 2 days of progress", () => {
+    const actual1 = updateProgress(day1);
+    expect(actual1).toStrictEqual(expectedDay1);
+
+    const actual2 = updateProgress(day2);
+    expect(actual2).toStrictEqual(expectedDay2);
+});
